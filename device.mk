ae8aacf419f8bb23d4750590c146c5c1ca631b88
khanfrd <khanfrd94@gmail.com>
2018-09-01 03:16:44 +0530

    whyred: disable ANT
    Change-Id: Icce095e9fcf218aff2b77b3819ca2eeb71b45943

--- device.mk
+++ device.mk
@@ -107,11 +107,11 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
 # ANT+
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     AntHalService \
     com.dsi.ant.antradio_library
 
-PRODUCT_COPY_FILES += \
+#PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Display
